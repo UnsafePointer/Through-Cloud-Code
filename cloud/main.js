c510c5b40f7fea0dbbf1579c707a597e4d944b6d
Renzo Crisostomo <ruenzuo@gmail.com>
2014-06-13 18:36:23 +0200

    Adding general feed generation to cloud job

--- cloud/main.js
+++ cloud/main.js
@@ -424,7 +424,7 @@ Parse.Cloud.job("generateFeeds", function(request, status) {
   query.find().then(function(users) {
     var promises = [];
     _.each(users, function(user) {
-      promises.push(generateFacebookFeed(user));
+      promises.push(generateFeedsForUser(user));
     });
     return Parse.Promise.when(promises);
   }, function(error) {
